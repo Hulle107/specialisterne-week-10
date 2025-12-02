af8625afdbbec1a5167168c5b19687e3de91b5bf
Delta Thiesen <delta.thiesen.1990@gmail.com>
2025-12-02 23:00:31 +0100

    Added tag support

--- src/services/hashid.service.ts
+++ src/services/hashid.service.ts
@@ -4,11 +4,14 @@ import Hashids from "hashids";
 const salt = process.env['HASHID_SALT'] || "missing_hashid_salt";
 const length = parseInt(process.env['HASHID_LENGTH'] || "10");
 const hashids = new Hashids(salt, length, 'abcdefghijklmnopqrstuvwxyz1234567890');
+const encoder = new TextEncoder();
 
-export function encodeId(id: number) {
-    return hashids.encode(id);
+export function encodeId(id: number, tag: string) {
+    let binary: number[] = [...encoder.encode(tag)];
+
+    return hashids.encode(id, ...binary);
 }
 
 export function decodeId(encoded: string) {
-    return hashids.decode(encoded);
+    return hashids.decode(encoded)[0];
 }
